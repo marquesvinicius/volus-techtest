76a944a5ff0470ec7869904d861987ececf4d303
Marques <marquesviniciusmelomartins@gmail.com>
2025-10-31 22:42:40 -0300

    adicionando nova porta permitida ao backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,6 +139,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # Vite dev server
     'http://127.0.0.1:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
